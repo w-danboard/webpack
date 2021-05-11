849e1b30ebf6c6efae652375df5e6276d288f23c
wanglin <wanglin46@lenovo.com>
2021-05-11 21:46:19 +0800

    style:补充webpack文件中注释

--- webpack/lib/webpack.js
+++ webpack/lib/webpack.js
@@ -3,27 +3,35 @@ const NodeEnvironmentPlugin = require('./node/NodeEnvironmentPlugin');
 const WebpackOptionsApply = require('./WebpackOptionsApply');
 
 /**
- * 
- * @param {*} options 选项 
+ * Webpack
+ * @param {*} options 配置参数
  * @param {*} callback 回调函数
  * @returns 编译器对象
  */
 const webpack = (options, callback) => {
-  /**
-   * 源码中的步骤 这里没有写
-   *  第一步验验证配置文件是否合法 如果不合法报错
-   *  第二步增加默认参数，因为webpack是零配置
-   */
-  let compiler = new Compiler(options.context); // 创建一个Compiler的实例
-  compiler.options = options; // 给他赋予一个options属性
-  new NodeEnvironmentPlugin().apply(compiler); // 让compiler可以读文件和写文件了
-  // 挂载配置文件里提供的所有的plugins
+  // 初始化参数，从配置文件和Shell语句中读取并合并参数。// TODO 待完善...
+  let shellOptions = process.argv.slice(2).reduce((config, args) => {
+    let [key, value] = args.split('=');
+    config[key.slice(2)] = value;
+    return config;
+  }, {});
+  options = {...options, ...shellOptions};
+  // 校验参数是否合法 // TODO
+
+  // 创建一个Compiler的实例
+  let compiler = new Compiler(options.context);
+  compiler.options = options;
+
+  // 让compiler可以读写文件
+  new NodeEnvironmentPlugin().apply(compiler);
+
+  // 挂载配置文件里提供的所有插件
   if (options.plugins && Array.isArray(options.plugins)) {
     for (const plugin of options.plugins) {
       plugin.apply(compiler);
     }
   }
-  // 初始化选项，挂载内置插件
+  // 挂载内置插件
   new WebpackOptionsApply().process(options, compiler);
   return compiler;
 };
